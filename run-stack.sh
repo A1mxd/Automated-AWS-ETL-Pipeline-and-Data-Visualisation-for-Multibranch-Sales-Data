3be552dfb5beb653dc32b6a73f6a2ec3e5b05ea2
lili-me <127961112+lili-me@users.noreply.github.com>
2023-05-03 14:30:32 +0100

    change to cloudformation deploy

--- run-stack.sh
+++ run-stack.sh
@@ -2,17 +2,6 @@
 
 set -eu
 
-# use YOUR OWN file name
-# use YOUR OWN name of the stack
-# use YOUR OWN profile name
-
-# aws cloudformation deploy --template-file marks-sample-stack.yml \
-# 		--stack-name marks-bucket-stack --profile generation-de \
-#          --region eu-west-1
-
-# aws cloudformation deploy --template-file cool-beans-aws-lambda-template.yml \
-# 		--stack-name cool-beans-final-project-stack --profile lili_me --region eu-west-1
-
-aws cloudformation create-stack --stack-name cool-beans-final-project \
-	--template-body file://cool-beans-aws-lambda-template.yml --region eu-west-1 \
+aws cloudformation deploy --stack-name cool-beans-final-project \
+	--template-file cool-beans-aws-lambda-template.yml --region eu-west-1 \
 	--capabilities CAPABILITY_IAM --profile lili_me

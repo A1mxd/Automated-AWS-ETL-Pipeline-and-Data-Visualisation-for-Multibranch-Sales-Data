d051075fb70cc66a891fca6701ec5d6d523d4449
lili-me <127961112+lili-me@users.noreply.github.com>
2023-05-03 14:32:42 +0100

    update for profile name for cloudformation

--- run-stack.sh
+++ run-stack.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+profile_name=$1
+
 aws cloudformation deploy --stack-name cool-beans-final-project \
 	--template-file cool-beans-aws-lambda-template.yml --region eu-west-1 \
-	--capabilities CAPABILITY_IAM --profile lili_me
+	--capabilities CAPABILITY_IAM --profile ${profile_name}

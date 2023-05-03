bc87ce1130785f14d8e11464db0db7ff7146afa8
Ahmed <Ahmedafr40@gmail.com>
2023-05-03 15:17:45 +0100

    updated the lambda function

--- src/cool-beans-csv-reader-lambda-function.py
+++ src/cool-beans-csv-reader-lambda-function.py
@@ -1,19 +1,20 @@
-import json
 import boto3
 import csv
 import os
-import datetime
+import logging
 
 s3 = boto3.client('s3')
-def extract_csv_from_bucket(bucket_name, file):
+def extract_csv_from_bucket(bucket_name, file_name):
     
     transaction_list = []
 
     try:
         
-        csv_file = s3.get_object(Bucket=bucket_name, Key=file)
-        c = csv_file['Body'].read().decode('utf-8').splitlines()
-        reader = csv.reader(c)
+        csv_file = s3.get_object(Bucket=bucket_name, Key=file_name)
+        print(f"Getting csv file: bucket name ={bucket_name}, key={file_name}")
+        transactions = csv_file['Body'].read().decode('utf-8').splitlines()
+        print(f"Read csv file: bucket name ={bucket_name}, key={file_name}")
+        reader = csv.reader(transactions)
 
         for line in reader:
                 transaction_entry = {
@@ -26,36 +27,29 @@ def extract_csv_from_bucket(bucket_name, file):
                     "card_number": line[6]
                     }
                 transaction_list.append(transaction_entry)
-                
-                
+        print(f'Extracted csv file: Rows = {len(transaction_list)}, bucket name = {bucket_name} ')        
         return transaction_list
+        
+    except Exception as e:
+        print(f"Lambda Extracting error = {e}")
     
-    except:
-        print(f"failed to open {file}")
-
 def lambda_handler(event, context):
     # TODO implement
+    try:
+        # file name inputted in the s3 bucket
+        file_name = event['Records'][0]['s3']['object']['key']
+        # folder_path = '2023/4/26/' #testing folder path
+        # file = folder_path + file_name
+        
+        bucket_name = event['Records'][0]['s3']['bucket']['name']
+        print(f'Lambda Handler: bucket name = {bucket_name}, file = {file_name}')
+        # print(f"delon9-cool-bean-csv-reader: invoked, event={event}")
+        list = extract_csv_from_bucket(bucket_name, file_name)
+        return {
+            'statusCode': 200,
+            'body': list
+        }
+    except Exception as e:
+        print(f"Lambda Handler Error = {e}") 
+
     
-    file_name = event['Records'][0]['s3']['object']['key']
-    folder_path = '2023/4/26/'
-    file = folder_path + file_name
-    
-    # time of the event triggered - to be able to get the folder located
-    time_event_str = event['Records'][0]["eventTime"]
-    time_event = datetime.datetime.strptime(time_event_str, "%Y-%m-%dT%H:%M:%S.%fZ")
-    year = time_event.year
-    year_str = str(year)
-    month = time_event.month
-    month_str = str(int(month))
-    day = time_event.day
-    day_str = str(int(day))
-    folder_path_triggered = f'{year_str}/{month_str}/{day_str}/'
-    # file = folder_path_triggered + file_name
-    
-    bucket_name = event['Records'][0]['s3']['bucket']['name']
-    # print(f"delon9-cool-bean-csv-reader: invoked, event={event}")
-    list = extract_csv_from_bucket(bucket_name, file)
-    return {
-        'statusCode': 200,
-        'body': list
-    }

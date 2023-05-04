17ed0fcad09d380d1d21de3ce02fc0a321f65198
Ahmed <Ahmedafr40@gmail.com>
2023-05-04 13:49:14 +0100

    update the lambda and the zip

--- src/cool-beans-csv-reader-lambda-function.py
+++ src/cool-beans-csv-reader-lambda-function.py
@@ -2,6 +2,9 @@ import boto3
 import csv
 import os
 import logging
+# import src.create_database as cdb
+import src.extract_transform as et
+# import src.load_database as db
 
 s3 = boto3.client('s3')
 def extract_csv_from_bucket(bucket_name, file_name):
@@ -32,24 +35,51 @@ def extract_csv_from_bucket(bucket_name, file_name):
         
     except Exception as e:
         print(f"Lambda Extracting error = {e}")
+        
     
 def lambda_handler(event, context):
     # TODO implement
     try:
         # file name inputted in the s3 bucket
         file_name = event['Records'][0]['s3']['object']['key']
-        # folder_path = '2023/4/26/' #testing folder path
-        # file = folder_path + file_name
         
         bucket_name = event['Records'][0]['s3']['bucket']['name']
-        print(f'Lambda Handler: bucket name = {bucket_name}, file = {file_name}')
+        print(f'Lambda Handler: bucket name = {bucket_name}, file = {file_name}') #CHECKS FOR CORRECT CSV FILE/BUCKET
         # print(f"delon9-cool-bean-csv-reader: invoked, event={event}")
-        list = extract_csv_from_bucket(bucket_name, file_name)
+        
+        # CREATING DATABASE
+        # connection = cdb.setup_db_connection()
+        # cdb.create_items_table(connection)
+        # cdb.create_payment_types_table(connection)
+        # cdb.create_locations_table(connection)
+        # cdb.create_transaction_table(connection)
+        # cdb.create_transaction_items_table(connection)
+        
+        #EXTRACTING 
+        transactions = extract_csv_from_bucket(bucket_name, file_name)
+        
+        # TRANSFORMING
+        sensitive_data = ["customer_name", "card_number"]
+        et.remove_sensitive_data(transactions, sensitive_data)
+        
+        baskets = et.create_item_list(transactions)
+
+        transactions = et.convert_all_dates(transactions, ['date_time'])
+
+        unique_items = et.get_unique_items(baskets)
+        
+        #LOADING DATABASE
+        # unique_locations = et.get_unique_locations(transactions)
+        # db.insert_into_location_table(connection, unique_locations)
+        # db.insert_into_item_table(connection, unique_items)
+        # db.insert_into_transactions_table(connection, transactions, baskets)
         return {
             'statusCode': 200,
-            'body': list
+            'body': baskets
         }
     except Exception as e:
         print(f"Lambda Handler Error = {e}") 
 
+    
+    
     

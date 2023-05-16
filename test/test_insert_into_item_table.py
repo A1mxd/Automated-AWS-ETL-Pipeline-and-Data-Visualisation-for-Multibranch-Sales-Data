7c92399b61c1e7f860c218acaa9b68337bdffae8
lili-me <127961112+lili-me@users.noreply.github.com>
2023-05-16 16:42:46 +0100

    WIP - test_insert_into_item_table

--- test/test_insert_into_item_table.py
+++ test/test_insert_into_item_table.py
@@ -1,69 +1,77 @@
-# from unittest.mock import call, MagicMock
-# from src.load_database import insert_into_item_table
+from unittest.mock import call, MagicMock, patch
+from src.load_database import insert_into_item_table
 
-
-# def test_inserts_new_item():
+@patch('builtins.print')
+def test_inserts_new_item(mock_print):
     
-#     connection = MagicMock()
-#     connection.cursor().fetchone.side_effect = [None]
+    connection = MagicMock()
+    connection.cursor().fetchone.side_effect = [None]
 
-#     unique_items_list = [{"item_name": "coffee",
-#                           "item_price": 1.35,
-#                           "temp_transaction_id": 1}]
+    unique_items_list = [{"item_name": "coffee",
+                          "item_price": 1.35,
+                          "temp_transaction_id": 1}]
 
-#     insert_into_item_table(connection, unique_items_list)
+    insert_into_item_table(connection, unique_items_list)
 
-#     connection.cursor().execute.assert_has_calls([
-#         call('''SELECT * FROM items WHERE item_name = 'coffee' AND item_price = '1.35' LIMIT (1);'''),
-#         call(''' INSERT INTO items(item_name, item_price)
-#                 VALUES (%s, %s);
-#                 ''', ('coffee', 1.35))])
-#     connection.commit.assert_called_once()
+    connection.cursor().execute.assert_has_calls([
+        call('''SELECT * FROM items WHERE item_name = 'coffee' AND item_price = '1.35' LIMIT (1);'''),
+        call(''' INSERT INTO items(item_name, item_price)
+                VALUES (%s, %s);
+                ''', ('coffee', 1.35))])
+    connection.commit.assert_called_once()
+
+    assert mock_print.call_count == 2
+    assert mock_print.call_args_list == [call('insert_into_item_table started'), 
+                                         call('insert_into_item_table completed')]
     
+@patch('builtins.print')
+def test_skips_existing_item(mock_print):
 
-# def test_skips_existing_item():
+    connection = MagicMock()
+    connection.cursor().fetchone.side_effect = [(1, "hot chocolate",2.35),(2, "tea",1.85)]
 
-#     connection = MagicMock()
-#     connection.cursor().fetchone.side_effect = [(1, "hot chocolate",2.35),(2, "tea",1.85)]
+    unique_items_list = [{"item_name": "hot chocolate",
+                          "item_price": 2.35,
+                          "temp_transaction_id": 1},
+                          {"item_name": "tea",
+                          "item_price": 1.85,
+                          "temp_transaction_id": 2}]
 
-#     unique_items_list = [{"item_name": "hot chocolate",
-#                           "item_price": 2.35,
-#                           "temp_transaction_id": 1},
-#                           {"item_name": "tea",
-#                           "item_price": 1.85,
-#                           "temp_transaction_id": 2}]
+    insert_into_item_table(connection, unique_items_list)
 
-#     insert_into_item_table(connection, unique_items_list)
+    connection.cursor().execute.assert_has_calls([
+        call('''SELECT * FROM items WHERE item_name = 'hot chocolate' AND item_price = '2.35' LIMIT (1);'''),
+        call('''SELECT * FROM items WHERE item_name = 'tea' AND item_price = '1.85' LIMIT (1);''')
+    ])
+    connection.commit.assert_not_called()
 
-#     connection.cursor().execute.assert_has_calls([
-#         call('''SELECT * FROM items WHERE item_name = 'hot chocolate' AND item_price = '2.35' LIMIT (1);'''),
-#         call('''SELECT * FROM items WHERE item_name = 'tea' AND item_price = '1.85' LIMIT (1);''')
-#     ])
-#     connection.commit.assert_not_called()
+    assert mock_print.call_count == 2
+    assert mock_print.call_args_list == [call('insert_into_item_table started'), 
+                                         call('insert_into_item_table completed')]
+  
 
+def test_skips_existing_items_and_inserts_new_items():
+    connection = MagicMock()
+    connection.cursor().fetchone.side_effect = [(1, "hot chocolate",2.35),None,(2, "tea",1.85)]
 
-# def test_skips_existing_items_and_inserts_new_items():
-#     connection = MagicMock()
-#     connection.cursor().fetchone.side_effect = [(1, "hot chocolate",2.35),None,(2, "tea",1.85)]
+    unique_items_list = [{"item_name": "hot chocolate",
+                          "item_price": 2.35,
+                          "temp_transaction_id": 1},
+                          {"item_name": "coffee",
+                          "item_price": 1.35,
+                          "temp_transaction_id": 1},
+                          {"item_name": "tea",
+                          "item_price": 1.85,
+                          "temp_transaction_id": 2}]
 
-#     unique_items_list = [{"item_name": "hot chocolate",
-#                           "item_price": 2.35,
-#                           "temp_transaction_id": 1},
-#                           {"item_name": "coffee",
-#                           "item_price": 1.35,
-#                           "temp_transaction_id": 1},
-#                           {"item_name": "tea",
-#                           "item_price": 1.85,
-#                           "temp_transaction_id": 2}]
+    insert_into_item_table(connection, unique_items_list)
 
-#     insert_into_item_table(connection, unique_items_list)
-
-#     connection.cursor().execute.assert_has_calls([
-#         call('''SELECT * FROM items WHERE item_name = 'hot chocolate' AND item_price = '2.35' LIMIT (1);'''),
-#         call('''SELECT * FROM items WHERE item_name = 'coffee' AND item_price = '1.35' LIMIT (1);'''),
-#         call(''' INSERT INTO items(item_name, item_price)
-#                 VALUES (%s, %s);
-#                 ''', ('coffee', 1.35)),
-#         call('''SELECT * FROM items WHERE item_name = 'tea' AND item_price = '1.85' LIMIT (1);''')])
+    connection.cursor().execute.assert_has_calls([
+        call('''SELECT * FROM items WHERE item_name = 'hot chocolate' AND item_price = '2.35' LIMIT (1);'''),
+        call('''SELECT * FROM items WHERE item_name = 'coffee' AND item_price = '1.35' LIMIT (1);'''),
+        call(''' INSERT INTO items(item_name, item_price)
+                VALUES (%s, %s);
+                ''', ('coffee', 1.35)),
+        call('''SELECT * FROM items WHERE item_name = 'tea' AND item_price = '1.85' LIMIT (1);''')])
         
-#     connection.commit.assert_called_once()
+    connection.commit.assert_called_once()

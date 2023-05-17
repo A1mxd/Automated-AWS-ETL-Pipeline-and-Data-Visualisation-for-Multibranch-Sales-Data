256671dec73e6e8195d708994192b8cf550b5cf6
Ahmed <Ahmedafr40@gmail.com>
2023-05-17 10:19:28 +0100

    fixed test_inserts_into_item_table

--- test/test_insert_into_item_table.py
+++ test/test_insert_into_item_table.py
@@ -8,16 +8,16 @@ def test_inserts_new_item(mock_print):
     connection.cursor().fetchone.side_effect = [None]
 
     unique_items_list = [{"item_name": "coffee",
-                          "item_price": 1.35,
+                          "item_price": '1.35' ,
                           "temp_transaction_id": 1}]
 
     insert_into_item_table(connection, unique_items_list)
 
     connection.cursor().execute.assert_has_calls([
-        call('''SELECT * FROM items WHERE item_name = 'coffee' AND item_price = '1.35' LIMIT (1);'''),
+        call("SELECT * FROM items WHERE item_name = 'coffee' AND item_price = '1.35' LIMIT (1);"),
         call(''' INSERT INTO items(item_name, item_price)
                 VALUES (%s, %s);
-                ''', ('coffee', 1.35))])
+                ''', ('coffee', '1.35'))])
     connection.commit.assert_called_once()
 
     assert mock_print.call_count == 2
@@ -28,13 +28,13 @@ def test_inserts_new_item(mock_print):
 def test_skips_existing_item(mock_print):
 
     connection = MagicMock()
-    connection.cursor().fetchone.side_effect = [(1, "hot chocolate",2.35),(2, "tea",1.85)]
+    connection.cursor().fetchone.side_effect = [(1, "hot chocolate",'2.35'),(2, "tea", '1.85')]
 
     unique_items_list = [{"item_name": "hot chocolate",
-                          "item_price": 2.35,
+                          "item_price": '2.35',
                           "temp_transaction_id": 1},
                           {"item_name": "tea",
-                          "item_price": 1.85,
+                          "item_price": '1.85',
                           "temp_transaction_id": 2}]
 
     insert_into_item_table(connection, unique_items_list)
@@ -52,16 +52,16 @@ def test_skips_existing_item(mock_print):
 
 def test_skips_existing_items_and_inserts_new_items():
     connection = MagicMock()
-    connection.cursor().fetchone.side_effect = [(1, "hot chocolate",2.35),None,(2, "tea",1.85)]
+    connection.cursor().fetchone.side_effect = [(1, "hot chocolate", '2.35'),None,(2, "tea", '1.85')]
 
     unique_items_list = [{"item_name": "hot chocolate",
-                          "item_price": 2.35,
+                          "item_price": '2.35',
                           "temp_transaction_id": 1},
                           {"item_name": "coffee",
-                          "item_price": 1.35,
+                          "item_price": '1.35',
                           "temp_transaction_id": 1},
                           {"item_name": "tea",
-                          "item_price": 1.85,
+                          "item_price": '1.85',
                           "temp_transaction_id": 2}]
 
     insert_into_item_table(connection, unique_items_list)
@@ -71,7 +71,7 @@ def test_skips_existing_items_and_inserts_new_items():
         call('''SELECT * FROM items WHERE item_name = 'coffee' AND item_price = '1.35' LIMIT (1);'''),
         call(''' INSERT INTO items(item_name, item_price)
                 VALUES (%s, %s);
-                ''', ('coffee', 1.35)),
+                ''', ('coffee', '1.35')),
         call('''SELECT * FROM items WHERE item_name = 'tea' AND item_price = '1.85' LIMIT (1);''')])
         
     connection.commit.assert_called_once()
